a4d0a08dfa6b06f1d2b13b2d66e29e1e5640963c
keerthanamurali612 <keerthanamurali099@gmail.com>
2023-11-28 18:30:44 +0530

    Get user input using prompt find the age

---
+++ age.js
@@ -0,0 +1,11 @@
+// Get user input using prompt(“Enter your age:”). 
+//If user is 18 or older ,give feedback:'You are old enough to drive' 
+// but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+let  find_Age=prompt("Enter your age:");
+if(find_Age>=18){
+    console.log('You are old enough to drive')
+}
+else{
+    console.log('You are not  old enough to drive')
+}
