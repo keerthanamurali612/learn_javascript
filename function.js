5f581b803675b6f11f1e67765d79e146b22b6a13
keerthanamurali612 <keerthanamurali099@gmail.com>
2023-11-28 18:44:53 +0530

    to find even or odd using function

---
+++ function.js
@@ -0,0 +1,11 @@
+// Check if a number is odd or even in JavaScript
+
+function EvenorOdd (num){
+    if(num%2==0){
+        console.log(`${num} is a even number`)
+    }
+    else{
+        console.log(`${num} is a odd number`)
+    }
+}
+EvenorOdd(67)
