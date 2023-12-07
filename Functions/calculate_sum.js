776db5038b6fd3f6e4cfc5bd0d6985319d143aef
keerthanamurali612 <keerthanamurali099@gmail.com>
2023-12-07 19:11:06 +0530

    sum of numbers

---
+++ Functions/calculate_sum.js
@@ -0,0 +1,8 @@
+// Write a JavaScript function to calculate the sum of two numbers. 
+
+function sum(a,b){
+    return a+b
+}
+
+const result= sum(45,30);
+console.log(`the sum of two numbers: ${result}`);
